446909ff992115c5fb1b69f8bf32657f504f7e0e
Kabi4 <hykukku@gmail.com>
2021-01-25 10:00:38 +0530

    Commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const server = require("./app");
+const server = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log(`Server are up and listening on port ${PORT}`);
+    console.log(`Server are up and listening on port, ${PORT}`);
 });
